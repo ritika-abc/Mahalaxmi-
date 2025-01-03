f76d1c669a0e95253923cca020968363f7db197b
ritika-abc <ritikasss321@gmail.com>
2025-01-03 15:09:56 +0530

    load

--- js/main.js
+++ js/main.js
@@ -80,6 +80,24 @@
             }
         }
     });
+    // Testimonials carousel
+    $(".benifit-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+    
+       
+        loop: true,
+        nav : true,
+        
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
 
 
     // Portfolio isotope and filter
